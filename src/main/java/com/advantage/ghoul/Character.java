e7ee9cf26024285e1ac73c7a7bf419870ffc8677
fabit0v <fabitov@amazon.com>
2022-10-25 09:55:59 -0700

    AB#338 added functionality to allow player to see inventory list -created method to allow player to add items to inventory

--- src/main/java/com/advantage/ghoul/Character.java
+++ src/main/java/com/advantage/ghoul/Character.java
@@ -21,6 +21,7 @@ class Character {
         test.check_Inventory();
     }
 
+    // --refactor to implement item menu
     private void check_Inventory() {
         System.out.println("Enter check bag to see inventory");
         String input = scanner.nextLine();
